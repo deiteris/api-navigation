8a78f03254f9b0150430e3cb29ee6eb5947ab66e
Pawel Psztyc <jarrodek@gmail.com>
2018-08-17 16:07:44 -0700

    Updating models, adding compact version

--- demo/model.js
+++ demo/model.js
@@ -1,5 +1,6 @@
 const amf = require('amf-client-js');
 const fs = require('fs');
+const jsonld = require('jsonld');
 
 amf.plugins.document.WebApi.register();
 amf.plugins.document.Vocabularies.register();
@@ -9,6 +10,57 @@ const files = new Map();
 files.set('demo-api/demo-api.raml', 'RAML 1.0');
 files.set('types-list/types-list.raml', 'RAML 1.0');
 files.set('exchange-experience-api/exchange-experience-api.raml', 'RAML 0.8');
+
+const ldContext = {
+  declares: 'http://a.ml/vocabularies/document#declares',
+  encodes: 'http://a.ml/vocabularies/document#encodes',
+  references: 'http://a.ml/vocabularies/document#references',
+  name: 'http://schema.org/name',
+  desc: 'http://schema.org/description',
+  srv: 'http://a.ml/vocabularies/http#server',
+  url: 'http://a.ml/vocabularies/http#url',
+  var: 'http://a.ml/vocabularies/http#variable',
+  pname: 'http://a.ml/vocabularies/http#paramName',
+  required: 'http://www.w3.org/ns/hydra/core#required',
+  binding: 'http://a.ml/vocabularies/http#binding',
+  schema: 'http://a.ml/vocabularies/http#schema',
+  scheme: 'http://a.ml/vocabularies/http#scheme',
+  dt: 'http://www.w3.org/ns/shacl#datatype',
+  pattern: 'http://www.w3.org/ns/shacl#pattern',
+  w3name: 'http://www.w3.org/ns/shacl#name',
+  def: 'http://www.w3.org/ns/shacl#defaultValue',
+  dvalue: 'http://a.ml/vocabularies/data#value',
+  in: 'http://www.w3.org/ns/shacl#in',
+  v: 'http://schema.org/version',
+  doc: 'http://schema.org/documentation',
+  title: 'http://schema.org/title',
+  ep: 'http://a.ml/vocabularies/http#endpoint',
+  path: 'http://a.ml/vocabularies/http#path',
+  op: 'http://www.w3.org/ns/hydra/core#supportedOperation',
+  method: 'http://www.w3.org/ns/hydra/core#method',
+  exp: 'http://www.w3.org/ns/hydra/core#expect',
+  par: 'http://a.ml/vocabularies/http#parameter',
+  items: 'http://a.ml/vocabularies/shapes#items',
+  ex: 'http://a.ml/vocabularies/document#examples',
+  strict: 'http://a.ml/vocabularies/document#strict',
+  svalue: 'http://a.ml/vocabularies/document#structuredValue',
+  rdfNumber: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#member',
+  raw: 'http://www.w3.org/ns/shacl#raw',
+  vdocname: 'http://a.ml/vocabularies/document#name',
+  cdip: 'http://a.ml/vocabularies/document#customDomainProperties',
+  dp: 'http://a.ml/vocabularies/document#DomainProperty',
+  de: 'http://a.ml/vocabularies/document#DomainElement',
+  shapesShape: 'http://a.ml/vocabularies/shapes#Shape',
+  shape: 'http://www.w3.org/ns/shacl#Shape',
+  scalar: 'http://a.ml/vocabularies/shapes#ScalarShape',
+  shapesSchema: 'http://a.ml/vocabularies/shapes#schema',
+  prop: 'http://www.w3.org/ns/shacl#property',
+  range: 'http://a.ml/vocabularies/shapes#range',
+  mc: 'http://www.w3.org/ns/shacl#minCount',
+  mac: 'http://www.w3.org/ns/shacl#maxCount',
+  node: 'http://www.w3.org/ns/shacl#NodeShape'
+};
+
 /**
  * Generates json/ld file from parsed document.
  *
@@ -25,7 +77,20 @@ function processFile(doc, file) {
     dest = dest.substr(dest.lastIndexOf('/'));
   }
   return generator.generateString(doc)
-  .then((data) => fs.writeFileSync('demo/' + dest, data, 'utf8'));
+  .then((data) => {
+    fs.writeFileSync('demo/' + dest, data, 'utf8');
+    return new Promise((resolve) => {
+      jsonld.compact(JSON.parse(data), ldContext, (err, compacted) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const f = 'demo/' + dest.replace('.json', '-compact.json');
+          fs.writeFileSync(f, JSON.stringify(compacted, null, 2), 'utf8');
+        }
+        resolve();
+      });
+    });
+  });
 }
 /**
  * Parses file and sends it to process.
