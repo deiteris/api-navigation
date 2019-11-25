e70098c99e0739dd4188c64a07430972cae840e8
Paweł Psztyć <jarrodek@gmail.com>
2019-11-24 19:18:59 -0800

    test: restoring tests

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -12,8 +12,8 @@ module.exports = (config) => {
     'SL_Firefox',
     'SL_Firefox-1',
     'SL_Safari',
-    // 'SL_Safari-1',
-    // 'SL_EDGE'
+    'SL_Safari-1',
+    'SL_EDGE'
   ];
   if (process.env.TRAVIS) {
     const buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
