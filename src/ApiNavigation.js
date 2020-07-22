4fa06f76d57f733fa3f8a3e7cd8de34961c3b491
Carolina Wright <carolina.wright@mulesoft.com>
2020-07-22 11:50:43 -0300

    fix: validate start of url in documentation

--- src/ApiNavigation.js
+++ src/ApiNavigation.js
@@ -925,18 +925,10 @@ export class ApiNavigation extends AmfHelperMixin(LitElement) {
     });
   }
 
-  _validUrlRegex = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  ); // fragment locator
-
   _validUrl(url) {
-    return this._validUrlRegex.test(url) ? url : 'about:blank';
+    return url.startsWith('http://') || url.startsWith('https://')
+      ? url
+      : 'about:blank';
   }
 
   /**
